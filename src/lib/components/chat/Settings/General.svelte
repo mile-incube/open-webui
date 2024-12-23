4c1bef07d0553c9026cc41383763e0e3583efde0
Steffen Hauptmann <s.hauptmann@mile-incube.com>
2024-12-23 17:34:41 +0100

    Remove not needed themes

--- src/lib/components/chat/Settings/General.svelte
+++ src/lib/components/chat/Settings/General.svelte
@@ -14,7 +14,7 @@
 	export let getModels: Function;
 
 	// General
-	let themes = ['dark', 'light', 'rose-pine dark', 'rose-pine-dawn light', 'oled-dark'];
+	let themes = ['dark', 'light'/*, 'rose-pine dark', 'rose-pine-dawn light', 'oled-dark'*/];
 	let selectedTheme = 'system';
 
 	let languages: Awaited<ReturnType<typeof getLanguages>> = [];
